ec6c4d207b64824feb80aabe38782be766397430
dasasdhba <dasasdhba@outlook.com>
2023-08-20 14:41:19 +0800

    add node utils

---
+++ cs/Utils/UNode.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+namespace Utils;
+
+/// <summary>
+/// Useful functions for node operation.
+/// </summary>
+public static partial class UNode
+{
+    /// <summary>
+    /// Copy children nodes as target node's children.
+    /// Make sure the child has a parameterless constructor.
+    /// Signal and group will be ignored.
+    /// </summary>
+    public static void CopyTo(Node source, Node target)
+    {
+        foreach (Node node in source.GetChildren())
+        {
+            Node duplicate = node.Duplicate((int)Node.DuplicateFlags.Scripts);
+            target.AddChild(duplicate);
+        }
+    }
+}
