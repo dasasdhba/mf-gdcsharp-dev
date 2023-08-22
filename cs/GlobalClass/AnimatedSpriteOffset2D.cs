8ae8f30ea4a0768573026611dc2198f6fad4a2a1
dasasdhba <dasasdhba@outlook.com>
2023-08-22 21:20:11 +0800

    change class name

---
+++ cs/GlobalClass/AnimatedSpriteOffset2D.cs
@@ -0,0 +1,57 @@
+﻿using Godot;
+using Godot.Collections;
+
+namespace GlobalClass;
+
+/// <summary>
+/// AnimatedSprite2D with offset setting.
+/// Please ignore AnimatedSprite's Offset property.
+/// </summary>
+[GlobalClass]
+public partial class AnimatedSpriteOffset2D :AnimatedSprite2D
+{
+    [ExportCategory("AnimatedSpriteOffset")]
+
+    [Export]
+    public Vector2 BaseOffset
+    {
+        get => _BaseOffset;
+        set
+        {
+            if (_BaseOffset != value)
+            {
+                _BaseOffset = value;
+                SetOffset();
+            }
+        }
+    }
+    private Vector2 _BaseOffset = new(0f, 0f);
+
+    public AnimatedSpriteOffset2D() : base()
+    {
+        AnimationChanged += SetOffset;
+        FrameChanged += SetOffset;
+        SpriteFramesChanged += SetOffset;
+    }
+
+    protected void SetOffset()
+    {
+        if (SpriteFrames is SpriteFramesOffset sprOffset)
+        {
+            Dictionary<string, SpriteFramesOffsetParam[]> offsetDict = sprOffset.Offsets;
+            if (offsetDict.ContainsKey(Animation))
+            {
+                foreach (SpriteFramesOffsetParam param in offsetDict[Animation]) 
+                {
+                    if (Frame == param.Frame) 
+                    { 
+                        Offset = BaseOffset + param.Offset;
+                        return;
+                    }
+                }
+            }
+        }
+
+        Offset = BaseOffset;
+    }
+}
