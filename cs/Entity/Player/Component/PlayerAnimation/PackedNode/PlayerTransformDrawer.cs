eb7e54d9e1b41223a8ced7f745b352d94637434b
dasasdhba <dasasdhba@outlook.com>
2023-08-23 20:36:49 +0800

    export name update

--- cs/Entity/Player/Component/PlayerAnimation/PackedNode/PlayerTransformDrawer.cs
+++ cs/Entity/Player/Component/PlayerAnimation/PackedNode/PlayerTransformDrawer.cs
@@ -11,7 +11,7 @@ public partial class PlayerTransformDrawer : Draw2D
     /// <summary>
     /// The PlayerStateAnimation Node.
     /// </summary>
-    [ExportCategory("PlayerAnimationTransform")]
+    [ExportCategory("PlayerTransformDrawer")]
     [Export]
     public PlayerStateAnimation StateAnimation { get; set; }
 
